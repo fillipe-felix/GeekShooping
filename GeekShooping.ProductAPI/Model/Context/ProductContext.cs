ccffc031774f7a110713fc7b96269cdd51eeb628
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-05 20:55:48 -0300

    Mapeando as Entidades do Banco de Dados com Entity Framework

--- GeekShooping.ProductAPI/Model/Context/ProductContext.cs
+++ GeekShooping.ProductAPI/Model/Context/ProductContext.cs
@@ -6,6 +6,7 @@ public class ProductContext : DbContext
 {
     public ProductContext(DbContextOptions<ProductContext> options) : base(options)
     {
-        
     }
+
+    public DbSet<Product> Products { get; set; }
 }
