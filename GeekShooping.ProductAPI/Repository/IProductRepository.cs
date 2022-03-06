07fc0143ed0981a0a3fe7367fa3f0e1a801b1273
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-05 22:56:29 -0300

    Definindo as Propriedades da Interface IProductRepository

--- GeekShooping.ProductAPI/Repository/IProductRepository.cs
+++ GeekShooping.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace GeekShooping.ProductAPI.Repository;
+﻿using GeekShooping.ProductAPI.Data.ViewModels;
+
+namespace GeekShooping.ProductAPI.Repository;
 
 public interface IProductRepository
 {
-    
+    Task<IEnumerable<ProductViewModel>> FindAll();
+    Task<ProductViewModel> FindById(string id);
+    Task<ProductViewModel> Create(ProductViewModel viewModel);
+    Task<ProductViewModel> Update(ProductViewModel viewModel);
+    Task<bool> Create(string id);
 }
