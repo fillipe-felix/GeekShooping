d1ac0079a42eae9a0925b33bafd47ac5422201b8
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-06 17:38:09 -0300

    Definindo a Estrutura Responsável por Consumir o Microsserviço de Produto Definindo as Operações da Interface IProductService

---
+++ GeekShooping.Web/Services/Interfaces/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using GeekShooping.Web.Models;
+
+namespace GeekShooping.Web.Services.Interfaces;
+
+public interface IProductService
+{
+    Task<IEnumerable<ProductViewModel>> FindAllProducts();
+    Task<ProductViewModel> FindProductById(Guid id);
+    Task<ProductViewModel> CreateProduct(ProductViewModel viewModel);
+    Task<ProductViewModel> UpdateProduct(Guid id, ProductViewModel viewModel);
+    Task<bool> DeleteProductById(Guid id);
+}
