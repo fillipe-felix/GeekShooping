3184ee2c8dc7de8979939936e2275f9c02c509aa
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-11 21:14:55 -0300

    Adicionando Configurações de Banco de Dados e Segurança

--- GeekShooping.IdentityServer/Program.cs
+++ GeekShooping.IdentityServer/Program.cs
@@ -1,7 +1,39 @@
+using GeekShooping.IdentityServer.Configuration;
+using GeekShooping.IdentityServer.Model;
+using GeekShooping.IdentityServer.Model.Context;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+// Add conexão do banco de dados do identityServer
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<IdentityServerContext>(opt =>
+{
+    opt.UseSqlServer(connection);
+});
+
+// Adicionando configurações de segurança do identity server
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityServerContext>()
+    .AddDefaultTokenProviders();
+
+var builderIdentityServer = builder.Services.AddIdentityServer(opt =>
+{
+    opt.Events.RaiseErrorEvents = true;
+    opt.Events.RaiseInformationEvents = true;
+    opt.Events.RaiseFailureEvents = true;
+    opt.Events.RaiseSuccessEvents = true;
+    opt.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+builderIdentityServer.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -17,7 +49,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
