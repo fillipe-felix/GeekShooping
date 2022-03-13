91983edcbc1593b3fd43565035d0694271c8153c
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-12 23:55:27 -0300

    alteração dos namespaces

---
+++ GeekShooping.IdentityServer/Views/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace GeekShooping.IdentityServer.Views.Account.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
