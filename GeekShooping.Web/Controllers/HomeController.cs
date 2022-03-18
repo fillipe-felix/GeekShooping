cdfcf63e4c4c52a1cd11c4f802455c511069e1db
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-17 22:06:36 -0300

    Ajustando o HomeController

--- GeekShooping.Web/Controllers/HomeController.cs
+++ GeekShooping.Web/Controllers/HomeController.cs
@@ -3,7 +3,9 @@
 using Microsoft.AspNetCore.Mvc;
 
 using GeekShooping.Web.Models;
+using GeekShooping.Web.Services.Interfaces;
 
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 
 namespace GeekShooping.Web.Controllers;
@@ -11,15 +13,20 @@ namespace GeekShooping.Web.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IProductService _productService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, 
+                          IProductService productService)
     {
         _logger = logger;
+        _productService = productService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var token = await HttpContext.GetTokenAsync("access_token");
+        var products = await _productService.FindAllProducts(token);
+        return View(products);
     }
 
     public IActionResult Privacy()
