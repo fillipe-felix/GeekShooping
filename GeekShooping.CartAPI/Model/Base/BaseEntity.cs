9d82095344f75b0a0b31f3f0e4f6016c36280ac9
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-19 17:58:34 -0300

    Configurando a Classe Startup e DbContext do GeekShopping.CartAPI MS

---
+++ GeekShooping.CartAPI/Model/Base/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShooping.CartAPI.Model.Base;
+
+public abstract class BaseEntity
+{
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    [Key]
+    [Column("id")]
+    public Guid Id { get; set; }
+}
---
+++ GeekShooping.CartAPI/Model/Context/CartApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShooping.CartAPI.Model.Context;
+
+public class CartApiContext : DbContext
+{
+    public CartApiContext(DbContextOptions<CartApiContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
