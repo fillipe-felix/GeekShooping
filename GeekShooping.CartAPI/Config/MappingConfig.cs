037e74845860805d3de4a6eed31a5e1b93db1647
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-20 11:26:36 -0300

    adicionando classe MappingConfig

---
+++ GeekShooping.CartAPI/Config/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+using GeekShooping.CartAPI.Model;
+
+namespace GeekShooping.CartAPI.Config;
+
+public class MappingConfig : Profile
+{
+    public MappingConfig()
+    {
+        // CreateMap<ProductViewModel, Product>().ReverseMap();
+        // CreateMap<ProductInputModel, Product>().ReverseMap();
+    }
+}
