47b16f57b6d47113ea558e4bf2ea6fdf751b4121
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-23 23:23:29 -0300

    Implementando o Controller Responsável por Expor os Endpoints de Carrinho

---
+++ GeekShooping.CartAPI/Controllers/CartController.cs
@@ -0,0 +1,52 @@
+﻿using GeekShooping.CartAPI.Data.ViewModels;
+using GeekShooping.CartAPI.Repositories.Interfaces;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.CartAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CartController : ControllerBase
+    {
+        private ICartRepository _repository;
+
+        public CartController(ICartRepository repository)
+        {
+            _repository = repository ?? throw new
+                ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet("find-cart/{id}")]
+        public async Task<ActionResult<CartViewModel>> FindById(Guid userId)
+        {
+            var cart = await _repository.FindCartByUserId(userId);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpPost("add-cart/{id}")]
+        public async Task<ActionResult<CartViewModel>> AddCart(CartViewModel vo)
+        {
+            var cart = await _repository.SaveOrUpdateCart(vo);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpPut("update-cart/{id}")]
+        public async Task<ActionResult<CartViewModel>> UpdateCart(CartViewModel vo)
+        {
+            var cart = await _repository.SaveOrUpdateCart(vo);
+            if (cart == null) return NotFound();
+            return Ok(cart);
+        }
+
+        [HttpDelete("remove-cart/{id}")]
+        public async Task<ActionResult<CartViewModel>> RemoveCart(Guid id)
+        {
+            var status = await _repository.RemoveFromCart(id);
+            if (!status) return BadRequest();
+            return Ok(status);
+        }
+    }
+}
