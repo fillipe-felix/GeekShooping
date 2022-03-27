21701ba7d7fbe6a7236c5038eceb3b6c27e1e813
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-27 14:42:50 -0300

    Adicionando o Carrinho de Compras ao Menu e Criando o CartController

---
+++ GeekShooping.Web/Controllers/CartController.cs
@@ -0,0 +1,59 @@
+﻿using GeekShooping.Web.Models;
+using GeekShooping.Web.Services.Interfaces;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShooping.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService,
+            ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await FindUserCart());
+        }
+
+        public async Task<IActionResult> Remove(string id)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.RemoveFromCart(id, token);
+
+            if(response)
+            {
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+                
+        private async Task<CartViewModel> FindUserCart()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.FindCartByUserId(Guid.Parse(userId), token);
+
+            if (response?.CartHeader != null)
+            {
+                foreach (var detail in response.CartDetails)
+                {
+                    response.CartHeader.PurchaseAmount += (double)(detail.Product.Price * detail.Count);
+                }
+            }
+            return response;
+        }
+    }
+}
