4bca07c488333aaf53bc3bf43fff0d7d349a1119
fillipe.felix <felipesoares_1993@hotmail.com>
2022-03-28 19:48:36 -0300

    Implementando o CouponController

--- GeekShooping.Web/Program.cs
+++ GeekShooping.Web/Program.cs
@@ -18,6 +18,11 @@ builder.Services.AddHttpClient<ICartService, CartService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"]);
 });
 
+// builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
+// {
+//     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]);
+// });
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultScheme = "Cookies";
