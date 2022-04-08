5c4c705563ffd411ac4d8ace733480d3accac5ee
fillipe.felix <felipesoares_1993@hotmail.com>
2022-04-08 20:42:08 -0300

    Implementando as Classes de Repositório do GeekShopping.OrderAPI

---
+++ GeekShopping.OrderAPI/Repository/OrderRepository.cs
@@ -0,0 +1,42 @@
+﻿using GeekShopping.OrderAPI.Model;
+using GeekShopping.OrderAPI.Model.Context;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly DbContextOptions<OrderApiContext> _context;
+
+        public OrderRepository(DbContextOptions<OrderApiContext> context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddOrder(OrderHeader header)
+        {
+            if (header == null)
+            {
+                return false;
+            }
+            
+            await using var _db = new OrderApiContext(_context);
+            _db.Headers.Add(header);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task UpdateOrderPaymentStatus(long orderHeaderId, bool status)
+        {
+            await using var _db = new OrderApiContext(_context);
+            var header = await _db.Headers.FirstOrDefaultAsync(o => o.Id == orderHeaderId);
+
+            if (header != null)
+            {
+                header.PaymentStatus = status;
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
