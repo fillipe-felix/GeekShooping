24ab5ac8bcfb649a351bfe18351bef1c123383ac
fillipe.felix <felipesoares_1993@hotmail.com>
2022-04-08 22:13:09 -0300

    Iniciando a Implementação do RabbitMQCheckoutConsumer

---
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQMessageConsumer.cs
@@ -0,0 +1,33 @@
+﻿using GeekShopping.OrderAPI.Repository;
+
+using RabbitMQ.Client;
+
+namespace GeekShopping.OrderAPI.MessageConsumer;
+
+public class RabbitMQMessageConsumer : BackgroundService
+{
+    private readonly OrderRepository _repository;
+    private IConnection _connection;
+    private IModel _channel;
+
+    public RabbitMQMessageConsumer(OrderRepository repository)
+    {
+        _repository = repository;
+        var factory = new ConnectionFactory
+        {
+            HostName = "localhost",
+            UserName = "guest",
+            Password = "guest"
+        };
+        
+        _connection = factory.CreateConnection();
+
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(queue: "checkoutqueue", false, false, false, null);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
