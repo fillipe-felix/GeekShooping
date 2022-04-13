e9a408a121be45d0919c80aba66dafeb3f7ca811
fillipe.felix <felipesoares_1993@hotmail.com>
2022-04-12 21:42:39 -0300

    Adicionando um RabbitMQMessageSender ao GeekShopping.OrderAPI

--- GeekShopping.OrderAPI/Program.cs
+++ GeekShopping.OrderAPI/Program.cs
@@ -1,5 +1,6 @@
 using GeekShopping.OrderAPI.MessageConsumer;
 using GeekShopping.OrderAPI.Model.Context;
+using GeekShopping.OrderAPI.RabbitMQSender;
 using GeekShopping.OrderAPI.Repository;
 
 using Microsoft.EntityFrameworkCore;
@@ -56,6 +57,7 @@ builderContext.UseSqlServer(connection);
 
 builder.Services.AddSingleton(new OrderRepository(builderContext.Options));
 builder.Services.AddHostedService<RabbitMQCheckoutConsumer>();
+builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
 
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer",options =>
