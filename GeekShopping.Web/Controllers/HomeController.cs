73103e6e0f4ee6554cf3cbbb1f403d86e762562a
fillipe.felix <felipesoares_1993@hotmail.com>
2022-04-19 20:58:09 -0300

    removendo using desnecessarios

--- GeekShopping.Web/Controllers/HomeController.cs
+++ GeekShopping.Web/Controllers/HomeController.cs
@@ -3,12 +3,7 @@ using GeekShopping.Web.Services.IServices;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GeekShopping.Web.Controllers
 {
