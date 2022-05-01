9ca6f71583b6ed14742b478710b502a3856a3f7d
fillipe.felix <felipesoares_1993@hotmail.com>
2022-05-01 13:01:08 -0300

    Definindo as Configurações de Base URL e Segurança no Ocelot

--- GeekShopping.APIGateway/Program.cs
+++ GeekShopping.APIGateway/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.IdentityModel.Tokens;
+
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer",options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
 
 var app = builder.Build();
 
@@ -16,10 +33,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseOcelot();
 
 app.Run();
